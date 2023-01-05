dae1bf9b357576372a065dcbf57deb37a722e19b
ricardosf <ricardosepu93@gmail.com>
2023-01-05 10:18:10 -0700

    finishing setup for gh-pages

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://socialmediaapp-production-1954.up.railway.app/",
+  baseURL: "https://socialmediaapp-production-1954.up.railway.app/api",
   withCredentials: true,
 });

b789baa4631817d762f90c36418d7c80f25aa403
ricardosf <ricardosepu93@gmail.com>
2023-01-05 10:05:16 -0700

    added only client side

--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getPosts, addPost } from "../controllers/post.js";
-
-const router = express.Router();
-
-router.get("/", getPosts);
-router.post("/", addPost);
-
-export default router;
